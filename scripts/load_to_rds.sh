7ca902464f227eac00fef41b8a39988866c4e2bc
neadex <neadex@pc-47.home>
2020-12-23 18:42:50 +0100

    .

--- scripts/load_to_rds.sh
+++ scripts/load_to_rds.sh
@@ -1,4 +1,8 @@
+HOST=$1
+USER=$2
+DATABASE=$3
+
 pv data/steam.sql.gz | gunzip | mysql \
-	-h steam-analysis.cvkio2p2ho8s.eu-west-3.rds.amazonaws.com \
-	-u admin \
-	-p steam_analysis
+	-h $HOST \
+	-u $USER \
+	-p $DATABASE
